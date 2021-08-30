26df6bc21817c62e9f857b8992ea2b7d8cdb9687
Jordan Chiyoka <jchiyoka@gmail.com>
2021-08-31 00:06:03 +0200

    HomeExerciseFour

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const data = require("./components/data");
-const demo = express(); //??
+const demo = express(); 
 
 demo.use(cors());
 
